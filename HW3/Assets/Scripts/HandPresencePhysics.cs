1b612183a818ab86dcd42fa0c0b9a34fef4ca70c
KalasioFR <kalasio.games1@gmail.com>
2024-03-04 20:00:37 +0200

    Fixed and improved hand physics

--- HW3/Assets/Scripts/HandPresencePhysics.cs
+++ HW3/Assets/Scripts/HandPresencePhysics.cs
@@ -7,10 +7,53 @@ public class HandPresencePhysics : MonoBehaviour
 
     public Transform target;
     private Rigidbody rb;
+    public Renderer nonPhysicalHand;
+    public float showNonPhysicalHandDistance = 0.05f;
+    private Collider[] handColliders;
+    private bool isGrabbed = false;
 
     void Start()
     {
         rb = GetComponent<Rigidbody>();
+        handColliders = GetComponentsInChildren<Collider>();
+    }
+
+    public void EnableHandCollider()
+    {
+        if (!isGrabbed)
+        {
+            foreach (var item in handColliders)
+            {
+                item.enabled = true;
+            }
+        }
+    }
+
+    public void EnableHandColliderDelay(float delay)
+    {
+        isGrabbed = false;
+        Invoke("EnableHandCollider", delay);
+    }
+
+    public void DisableHandCollider()
+    {
+        isGrabbed = true;
+        foreach (var item in handColliders)
+        {
+            item.enabled = false;
+        }
+    }
+
+    private void Update()
+    {
+        float distance = Vector3.Distance(transform.position, target.position);
+
+        if (distance > showNonPhysicalHandDistance)
+        {
+            nonPhysicalHand.enabled = true;
+        }
+        else
+            nonPhysicalHand.enabled = false;
     }
 
     void FixedUpdate()
