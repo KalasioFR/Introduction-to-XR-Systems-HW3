4b00cce54096431b3d3695341657d0da813095af
KalasioFR <kalasio.games1@gmail.com>
2024-03-06 20:22:17 +0200

    Improved movement, added start menu and decorations

---
+++ HW3/Assets/Scripts/SetTurnTypeFromPlayerPref.cs
@@ -0,0 +1,38 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.XR.Interaction.Toolkit;
+
+public class SetTurnTypeFromPlayerPref : MonoBehaviour
+{
+    public ActionBasedContinuousTurnProvider continuousTurn;
+    public ActionBasedSnapTurnProvider snapTurn;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        ApplyPlayerPref();
+    }
+
+    public void ApplyPlayerPref()
+    {
+        if(PlayerPrefs.HasKey("turn"))
+        {
+            int value = PlayerPrefs.GetInt("turn");
+            if(value == 0)
+            {
+                continuousTurn.leftHandTurnAction.action.Enable();
+                continuousTurn.rightHandTurnAction.action.Enable();
+                snapTurn.leftHandSnapTurnAction.action.Disable();
+                snapTurn.rightHandSnapTurnAction.action.Disable();
+            }
+            else if(value == 1)
+            {
+                continuousTurn.leftHandTurnAction.action.Disable();
+                continuousTurn.rightHandTurnAction.action.Disable();
+                snapTurn.leftHandSnapTurnAction.action.Enable();
+                snapTurn.rightHandSnapTurnAction.action.Enable();
+            }
+        }
+    }
+}
